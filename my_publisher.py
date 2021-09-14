0119e7f9abebb81970136b2949f3bcfa6e160a88
Senh Mo Chuang <senhmo@gmail.com>
2021-09-13 20:33:22 -0500

    Added Streaming Turbine Data

--- my_publisher.py
+++ my_publisher.py
@@ -1,5 +1,12 @@
 import paho.mqtt.client as paho
 from  Confidential.mqtt_account import user, password, broker, port
+import pandas as pd
+from pathlib import Path
+from datetime import datetime
+from time import sleep
+
+turbine_df = pd.read_csv(Path.cwd().joinpath('turbine.csv'))
+
 
 def on_publish(client,userdata,result): 
     print(result)            #create function for callback
@@ -9,5 +16,7 @@ client1= paho.Client("control1")                           #create client object
 client1.on_publish = on_publish                          #assign function to callback
 client1.username_pw_set(user, password)
 client1.connect(broker,port)                                 #establish connection
-results, mid= client1.publish(f"{user}/you","It works!")                   #publish
-results, mid= client1.publish(f"{user}/you","It works again!")                   #publish
+for index, row in turbine_df.iterrows():
+    message = ';'.join((str(datetime.strftime(datetime.now(), '%c')), str(row['Tb_model']), str(row['Tb_mod_n'])))
+    results, mid= client1.publish(f"{user}/you",message)    
+    sleep(1)
